253b2ed8de9d83e7eefbc35fcd8ef1ee16b5b171
Neuboi <sadjei1@lsu.edu>
2019-04-10 14:58:07 -0500

    Day Two? 4/10

--- pixLab/classes/Picture.java
+++ pixLab/classes/Picture.java
@@ -98,6 +98,65 @@ public class Picture extends SimplePicture
     }
   }
   
+  public void keepOnlyBlue()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        pixelObj.setRed(0);
+        pixelObj.setGreen(0);
+      }
+    }
+  }
+  
+  public void negate()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        pixelObj.setRed(255 - pixelObj.getRed());
+        pixelObj.setGreen(255 - pixelObj.getGreen());
+        pixelObj.setBlue(255 - pixelObj.getBlue());
+      }
+    }
+  }
+  
+  public void greyscale()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        
+        int avgVal = (pixelObj.getRed() + pixelObj.getGreen() + pixelObj.getBlue())/3;
+        pixelObj.setRed(avgVal);
+        pixelObj.setGreen(avgVal);
+        pixelObj.setBlue(avgVal);
+      }
+    }
+  }
+
+   public void fixUnderwater()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    for (Pixel[] rowArray : pixels)
+    {
+      for (Pixel pixelObj : rowArray)
+      {
+        
+        pixelObj.setRed(pixelObj.getRed() - 255);
+        pixelObj.setGreen(pixelObj.getGreen() - 100);
+        pixelObj.setBlue(pixelObj.getBlue() - 90);
+      }
+    }
+  }
+  
+  
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
@@ -223,12 +282,11 @@ public class Picture extends SimplePicture
    */
   public static void main(String[] args) 
   {
-    DigitalPicture p = new DigitalPicture();
     
-    //Picture beach = new Picture("beach.jpg");
-    //beach.explore();
-    //beach.zeroBlue();
-    //beach.explore();
+    Picture beach = new Picture("beach.jpg");
+    beach.explore();
+    beach.zeroBlue();
+    beach.explore();
   }
   
 } // this } is the end of class Picture, put all new methods before this
